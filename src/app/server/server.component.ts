3880e3a0d06e2bdad19db9c08123d97afe9d7801
Varun <imva508@gmail.com>
2019-03-30 23:45:35 +0530

    ...

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styles: [`h1{
+        color: red;
+    }`]
+})
+export class ServerComponent {
+}
